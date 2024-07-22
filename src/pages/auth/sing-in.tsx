138258fe7d566991cee5a3e549d646dbe892e42b
Shindhi <diogoshindhinaka@gmail.com>
2024-07-22 18:56:46 -0300

    adiciona pagina de cadastro de um novo restaurante

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -11,16 +12,16 @@ const signInForm = z.object({
   email: z.string().email(),
 })
 
-type SingInForm = z.infer<typeof signInForm>
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SingInForm>()
+  } = useForm<SignInForm>()
 
-  async function handleSignIn(data: SingInForm) {
+  async function handleSignIn(data: SignInForm) {
     try {
       toast.success('Enviamos um link de autenticação no seu e-mail.', {
         action: {
@@ -37,6 +38,10 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
