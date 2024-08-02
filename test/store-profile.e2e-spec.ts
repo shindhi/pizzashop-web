c546eefd3a778b2eff0ae5a35facab52e9ec0abe
Shindhi <diogoshindhinaka@gmail.com>
2024-08-01 22:02:18 -0300

    adiciona um ajuste nos testes

--- test/store-profile.e2e-spec.ts
+++ test/store-profile.e2e-spec.ts
@@ -15,9 +15,9 @@ test('update profile successfully', async ({ page }) => {
 
   const toast = page.getByText('Perfil atualizado com sucesso!')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 
   await page.getByRole('button', { name: 'Close' }).click()
 
-  expect(page.getByRole('button', { name: 'Rocket Pizza' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Rocket Pizza' })).toBeVisible()
 })
